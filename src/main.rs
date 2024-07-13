3e49459748bca98bb90d24dac51d8906501b5c40
DrunkenCloud <drunkencloud99@gmail.com>
2024-07-13 16:40:27 +0530

    Simple insert and select functionality

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,113 @@
-struct Node {
-    data: i32,
-    next: Option<Box<Node>>,
+use std::env;
+use std::io::{self, Write};
+
+fn get_input() -> String {
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input
 }
 
-impl Node {
-    fn new(data: i32) -> Node {
-        Node {
-            data,
-            next: None,
-        }
-    }
+struct Line {
+	key: i32,
+	name: String,
+	email: String
 }
 
-struct LinkedList {
-    head: Option<Box<Node>>,
+impl Line {
+	fn new(key: i32, name: String, email: String) -> Line {
+		Line {
+			key: key,
+			name: name,
+			email: email
+		}
+	}
 }
 
-impl LinkedList {
-    fn new () -> LinkedList {
-        LinkedList { head: None }
-    }
+fn handle_input(input: &str, lines: &mut Vec<Line>) {
+	let parts: Vec<&str> = input.split_whitespace().collect();
+	if parts.len() != 4 {
+		print!("Invalid insert command. Use: insert <key> <name> <email>");
+		return;
+	}
+	let key: i32 = match parts[1].parse() {
+		Ok(num) => num,
+		Err(_) => {
+			print!("Invalid key. It should be an integer");
+			return;
+		}
+	};
 
-    fn push(&mut self, data: i32) {
-        let new_node = Box::new(Node::new(data));
-        if let Some(old_head) = self.head.take() {
-            self.head = Some(Box::new(Node {
-                data: new_node.data,
-                next: Some(old_head),
-            }));
-        } else {
-            self.head = Some(new_node);
-        }
-    }
+	let name = parts[2].to_string();
+	let email = parts[3].to_string();
+	let new_line = Line::new(key, name, email);
+	lines.push(new_line);
+}
+fn handle_select(input: &str, lines: &Vec<Line>) {
+	let parts: Vec<&str> = input.split_whitespace().collect();
+	if parts.len() != 2 {
+		println!("Invalid select command. Use select <key>");
+		return;
+	}
 
-    fn append(&mut self, data: i32) {
-        let new_node = Box::new(Node::new(data));
-        let mut current = &mut self.head;
+	let key: i32 = match parts[1].parse() {
+	    Ok(num) => num,
+	    Err(_) => {
+	    	println!("Invalid key. It should be an integer.");
+	    	return;
+	    }
+	};
 
-        while let Some(ref mut node) = current {
-            current = &mut node.next;
-        }
+	for line in lines {
+		if line.key == key {
+			println!("Found: {} {} {}",line.key, line.name, line.email);
+			return;
+		}
+	}
 
-        *current = Some(new_node);
-    }
+	println!("No entry found with key: {}", key);
+}
 
-    fn bottom(&mut self) -> Option<i32> {
-        self.head.take().map(|node| {
-            self.head = node.next;
-            node. data
-        })
-    }
+fn process_input(input: &str, lines: &mut Vec<Line>) -> bool {
+	let trimmed_input = input.trim().to_lowercase();
 
-    fn top(&mut self) -> Option<i32> {
-        let mut current = &mut self.head;
-        while let Some(ref mut node) = current {
-            if node.next.is_none() {
-                return Some(node.data);
-            }
-            current = &mut node.next;
-        }
-        None
-    }
-
-    fn remove(&mut self, data: i32) -> bool {
-        let mut current = &mut self.head;
-        while let Some(ref mut node) = current {
-            if let Some(ref mut next_node) = node.next {
-                if next_node.data == data {
-                    node.next = next_node.next.take();
-                    return true;
-                } else {
-                    if node.data == data {
-                        self.head = node.next.take();
-                        return true;
-                    }
-                }
-            }
-            current = &mut node.next;
-        }
-        false
-    }
-
-    fn pop(&mut self) -> bool {
-        let mut current = &mut self.head;
-        while let Some(ref mut node) = current {
-            if let Some(ref mut next_node) = node.next {
-                if next_node.next.is_none() {
-                    node.next = next_node.next.take();
-                    return true;
-                }
-            }
-            current = &mut node.next;
-        }
-        false
-    }
-
-    fn print(&self) {
-        let mut current = &self.head;
-        while let Some(node) = current {
-            print!("{} -> ", node.data);
-            current = &node.next;
-        }
-        println!("None");
-    }
+	if trimmed_input == "exit" {
+		println!("Exiting Program!");
+		return true;
+	} else if trimmed_input.starts_with("insert") {
+		println!("Performing insert command!");
+		handle_input(input, lines);
+	} else if trimmed_input.starts_with("select") {
+		println!("Performing select command!");
+		handle_select(input, lines);
+	} else {
+		println!("Unrecognied command");
+	}
+	false
 }
 
 fn main() {
-    let mut ll = LinkedList::new();
-    ll.push(1);
-    ll.push(2);
-    ll.append(3);
-    ll.append(4);
+    let args: Vec<String> = env::args().collect();
 
-    ll.print();
-
-    if let Some(value) = ll.bottom() {
-        println!("Popped value: {}", value);
-        ll.remove(value);
+    if args.len() > 1 {
+        for (i, arg) in args.iter().enumerate() {
+            if i == 0 {
+                continue;
+            }
+            println!("Argument: {}", arg);
+        }
     } else {
-        println!("List is empty");
+        println!("No arguments given");
     }
-    ll.print();
 
-    if let Some(value) = ll.top() {
-        println!("Top value: {}", value);
-        ll.pop();
-    } else {
-        println!("List is empty");
+    let mut lines: Vec<Line> = Vec::new();
+
+    loop {
+        print!("db > ");
+        let input: String = get_input();
+        if process_input(&input, &mut lines) {
+        	break;
+        }
     }
-    ll.print();
 }
